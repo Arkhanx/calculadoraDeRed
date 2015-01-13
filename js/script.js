d2443f61df5e45b516ad7bda981ca27832c3e907
arkhanx <efcaro91@gmail.com>
2015-01-13 16:22:51 -0500

    Añadido mascara de red con algunos fallos por corregir

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 function Calcular(){
     
     //Definición de Variables:
-    var octeto1, octeto2, octeto3, octeto4,
-        dato1, dato2, dato3, dato4, clase;
+    var dato1, dato2, dato3, dato4, clase,
+        octeto1, octeto2, octeto3, octeto4,
+        mascara1, mascara2, mascara3, mascara4, 
+        mascarabin1, mascarabin2, mascarabin3, mascarabin4,
+        mascarabininv1, mascarabininv2, mascarabininv3, mascarabininv4;
     
-    //Tomamos lo datos de los Inputs, en las variables octeto# y las convertimos a binario haciendo .toString con base 2:
+    /*Tomamos lo datos de los Inputs, en las variables octeto# y las convertimos 
+    a binario haciendo .toString con base 2: */
     
     dato1 = parseInt(document.getElementById('decimal1').value);
     dato2 = parseInt(document.getElementById('decimal2').value);
@@ -40,20 +44,64 @@ function Calcular(){
     }
     
     
-    /*Le añadimos 7 ceros por delante al resultado de octeto# y mostramos solo los 8 primero digitos de izquierda
-    a derecha, osea si octeto1 es igual a 101 entonces "0000000" + 101 = 0000000101 pero solo se mostrara 00000101
-    gracias a .slice(-8); : */
+    /*Le añadimos 7 ceros por delante al resultado de octeto# y mostramos solo los 8 
+    primeros digitos de izquierda a derecha, osea si octeto1 es igual a 101 entonces 
+    "0000000" + 101 = 0000000101 pero solo se mostrara 00000101gracias a .slice(-8);*/
     
     octeto1=("0000000" + octeto1).slice(-8);
     octeto2=("0000000" + octeto2).slice(-8);
     octeto3=("0000000" + octeto3).slice(-8);
     octeto4=("0000000" + octeto4).slice(-8);
-    
-console.log (octeto1.length);
         
-    //Escribe en el documento html en <p id="resultado"> el resultado de los numero convertidos a binario:
+    /* Escribe en el documento html en <p id="resultado"> el resultado de los numero 
+    convertidos a binario: */
     
     document.getElementById("resultado").innerHTML = octeto1 + " - " + octeto2 + " - " +
     octeto3 + " - " + octeto4;
+    
+    
+    mascaracorta = parseInt(document.getElementById('mascaracorta').value);
+    
+    
+    
+    mascarabin1 = ("00000000" + "11111111111111111111111111111111111").slice(0+mascaracorta, 8+mascaracorta);   
+    
+    //console.log(mascarabin1);
+    
+    mascarabininv1 = mascarabin1.split("").reverse().join("");
         
+    mascara1 = parseInt(0 + mascarabininv1.substring(0, 8),2);
+    document.getElementById("mascara1").value = mascara1;
+    
+    //console.log(mascara1);
+    
+    if(mascara1 == 255){
+        
+        mascaracorta = mascaracorta - 8;    
+        mascarabin2 = ("00000000" + "11111111111111111111111111111111111").slice(0+mascaracorta, 8+mascaracorta);
+        mascarabininv2 = mascarabin2.split("").reverse().join("");
+        mascara2 = parseInt(0 + mascarabininv2.substring(0, 8),2);
+        document.getElementById("mascara2").value = mascara2;
+    }
+            
+    if(mascara2 == 255){                    
+        mascaracorta = mascaracorta - 8;    
+        mascarabin3 = ("00000000" + "11111111111111111111111111111111111").slice(0+mascaracorta, 8+mascaracorta);
+        mascarabininv3 = mascarabin3.split("").reverse().join("");
+        mascara3 = parseInt(0 + mascarabininv3.substring(0, 8),2);
+        document.getElementById("mascara3").value = mascara3;
+        
+    }
+    
+    if(mascara3 == 255){                    
+        mascaracorta = mascaracorta - 8;    
+        mascarabin4 = ("00000000" + "11111111111111111111111111111111111").slice(0+mascaracorta, 8+mascaracorta);
+        mascarabininv4 = mascarabin4.split("").reverse().join("");
+        mascara4 = parseInt(0 + mascarabininv4.substring(0, 8),2);
+        document.getElementById("mascara4").value = mascara4;
+        
+    }
+    
+    
+    //console.log(mascarabin.substring(0, 8).length)
 };
